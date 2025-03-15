d67bfae75969dc836eee5411b0d1d698db0b0f09
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2025-03-15 11:30:55 +0100

    Amélioration des classes Soldier, Viking et Saxon : ajout de la gestion des dégâts et des méthodes d'attaque, ainsi que la mise à jour des constructeurs.

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,60 +1,67 @@
 import random
 
 # Soldier
-
-
 class Soldier:
     def __init__(self, health, strength):
-        # your code here
+        self.health = health
+        self.strength = strength
     
     def attack(self):
-        # your code here
+        return self.strength
 
     def receiveDamage(self, damage):
-        # your code here
-    
+        self.health -= damage
 
 # Viking
-
 class Viking(Soldier):
     def __init__(self, name, health, strength):
-        # your code here
+        super().__init__(health, strength)
+        self.name = name
+        self.health = health
+        self.strength = strength
 
     def battleCry(self):
-        # your code here
+        return f"Odin Owns You All!"
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
 
 # Saxon
-
 class Saxon(Soldier):
     def __init__(self, health, strength):
-        # your code here
+        super().__init__(health, strength)
 
     def receiveDamage(self, damage):
-        # your code here
-
-# Davicente
+        self.health -= damage
+        if self.health > 0:
+            return f"A Saxon has has received {damage} points of damage"
+        else:
+            return f"A Saxon has died in combat"
 
 class War():
     def __init__(self):
-        # your code here
+        self.vikingArmy = []
+        self.saxonArmy = []
 
-    def addViking(self, viking):
-        # your code here
+    def addViking(self, viking: Viking):
+        self.vikingArmy.append(viking)
     
-    def addSaxon(self, saxon):
-        # your code here
+    def addSaxon(self, saxon: Saxon):
+        self.saxonArmy.append(saxon)
     
     def vikingAttack(self):
-        # your code here
+        rand_saxon = self.saxonArmy[random.randint(0, len(self.saxonArmy) - 1)]
+        rand_viking = self.vikingArmy[random.randint(0, len(self.vikingArmy) - 1)]
+        rand_saxon.receiveDamage(rand_viking.strength)
+        
     
-    def saxonAttack(self):
-        # your code here
+    # def saxonAttack(self):
+    #     # your code here
 
-    def showStatus(self):
-        # your code here
-    pass
-
-
+    # def showStatus(self):
+    #     # your code here
+    # pass
